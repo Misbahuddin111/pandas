c82ffc6c6f0750f5122260d87aa3517449d2b765
Misbahuddin <misbahdin63@gmail.com>
2025-05-31 14:22:48 +0500

    pandas

--- 16 sessiion pandas series.py
+++ 16 sessiion pandas series.py
@@ -13,3 +13,30 @@ print(pd.Series(numbers))
 marks = [55,64,74,46,75]
 subjects = ["maths","urdu","english","physics","computer"]
 print(pd.Series(marks , index=subjects, name="Report card"))
+
+# series in dict 
+data = {
+    "name": "misbah",
+    "semester": "7th",
+    "roll no": 448,
+    "Department": "IBMS",
+}
+data_series = pd.Series(data ,name= "misbah data")
+print(data_series)
+
+# series attributes
+
+# size 
+print("size :",data_series.size)
+
+#dtype
+print(f"dtype : {data_series.dtype}")
+# name
+print(f"name : {data_series.name}")
+# is_unique
+print(f"is_unique : {data_series.is_unique}")
+# index 
+print(f"index : {data_series.index}")
+
+# value 
+print(f"values : {data_series.values}")
